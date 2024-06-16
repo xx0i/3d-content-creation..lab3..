25aba5a7c55f63c11adcf090ca36f788e3f1b5ec
Lari <lnorri.fullsail@gmail.com>
2024-06-16 13:17:08 -0400

    auto commit

---
+++ FragmentShader.hlsl
@@ -0,0 +1,5 @@
+// an ultra simple hlsl fragment shader
+float4 main() : SV_TARGET
+{
+	return float4(0.75f ,0.75f, 0.25f, 0); // TODO: Part 1a (optional)
+}
---
+++ VertexShader.hlsl
@@ -0,0 +1,12 @@
+// an ultra simple hlsl vertex shader
+// TODO: Part 1c
+// TODO: Part 2b
+// TODO: Part 2f
+// TODO: Part 3b
+float4 main(float2 inputVertex : POSITION) : SV_POSITION
+{
+	// TODO: Part 2d
+	// TODO: Part 2f
+	// TODO: Part 3b
+	return float4(inputVertex, 0, 1);
+}
