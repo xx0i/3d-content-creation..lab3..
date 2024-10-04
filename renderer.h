050d0760b10751c1ae2e68e56111c0c83a42f4d6
unknown <natalie9705@gmail.com>
2024-10-04 15:57:02 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -107,16 +107,16 @@ private:
 		int step = 0.04f;
 		for (int i = 0; i <= 25; i++)//horizontal lines
 		{
-			float y = -0.5f + i * step;
+			float z = -0.5f + i * step;
 			verts[index].x = -0.5f;
-			verts[index].y = y;
-			verts[index].z = 0.0f;
+			verts[index].y = 0.0f;
+			verts[index].z = z;
 			verts[index].w = 1.0f;
 			index++;
 
 			verts[index].x = 0.5f;
-			verts[index].y = y;
-			verts[index].z = 0.0f;
+			verts[index].y = 0.0f;
+			verts[index].z = z;
 			verts[index].w = 1.0f;
 			index++;
 		}
@@ -125,14 +125,14 @@ private:
 		{ 
 			float x = -0.5f + i * step;
 			verts[index].x = x;
-			verts[index].y = -0.5f;
-			verts[index].z = 0.0f;
+			verts[index].y = 0.0f;
+			verts[index].z = -0.5f;
 			verts[index].w = 1.0f;
 			index++;
 
 			verts[index].x = x;
-			verts[index].y = 0.5f;
-			verts[index].z = 0.0f;
+			verts[index].y = 0.0f;
+			verts[index].z = 0.5f;
 			verts[index].w = 1.0f;
 			index++;
 		}
