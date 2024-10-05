ad49c06ed79f7eac7ecbedf1ff70f0ec4805bfc6
unknown <natalie9705@gmail.com>
2024-10-04 23:27:41 -0400

    auto commit

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -16,7 +16,8 @@ cbuffer shaderVars
 float4 main(VERTEX input) : SV_POSITION
 {
 	// TODO: Part 2i
+    return mul(input.pos, worldMatrix);
 	// TODO: Part 3b
 	// TODO: Part 3g
-	return input.pos;
+	//return input.pos;
 }
--- renderer.h
+++ renderer.h
@@ -593,6 +593,7 @@ public:
 		firstWorldMatrix.worldMatrix = worldMatrix1;
 
 		// TODO: Part 2i // TODO: Part 4y
+		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets, 0, nullptr);
 
 		// TODO: Part 3g
 		vkCmdDraw(commandBuffer, 104, 1, 0, 0); // TODO: Part 1b 
