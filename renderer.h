c952c52426ef6c3bc0405cc4cc4a4294c60d31d1
unknown <natalie9705@gmail.com>
2024-10-07 17:48:15 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -105,8 +105,8 @@ public:
 		GW::MATH::GMATRIXF translationMatrix = GW::MATH::GIdentityMatrixF;
 		GW::MATH::GVECTORF translationVector = { 0.25f, -0.125f, -0.25f, 1.0f };
 		interfaceProxy.TranslateGlobalF(translationMatrix, translationVector, translationMatrix);
-		interfaceProxy.RotateYGlobalF(translationMatrix, -3.9f, translationMatrix);
-		interfaceProxy.RotateXGlobalF(translationMatrix, 1.8f, translationMatrix);
+		//interfaceProxy.RotateYGlobalF(translationMatrix, -3.9f, translationMatrix);
+		//interfaceProxy.RotateXGlobalF(translationMatrix, 1.8f, translationMatrix);
 		interfaceProxy.RotateZGlobalF(translationMatrix, G_DEGREE_TO_RADIAN_F(45), translationMatrix);
 
 		interfaceProxy.InverseF(translationMatrix, viewMatrix);
