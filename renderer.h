22d0da38be964bd4f130049147ca48031d0fa044
unknown <natalie9705@gmail.com>
2024-10-07 21:24:34 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -139,8 +139,8 @@ public:
 		//wall 1
 		rotationMatrix = GW::MATH::GIdentityMatrixF;
 		translationMatrix = GW::MATH::GIdentityMatrixF;
-		GW::MATH::GVECTORF wall1Translation = { 0.0f, 0.0f, 0.5f, 1.0f };
-		interfaceProxy.RotateZGlobalF(rotationMatrix, G_DEGREE_TO_RADIAN_F(180), rotationMatrix);
+		GW::MATH::GVECTORF wall1Translation = { 0.0f, -0.5f, 0.0f, 1.0f };
+		interfaceProxy.RotateZGlobalF(rotationMatrix, G_DEGREE_TO_RADIAN_F(90), rotationMatrix);
 		interfaceProxy.TranslateGlobalF(translationMatrix, wall1Translation, translationMatrix);
 		interfaceProxy.MultiplyMatrixF(rotationMatrix, translationMatrix, worldMatrix3);
 		shaderVarsUniformBuffer.worldMatrix[2] = worldMatrix3;
