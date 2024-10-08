2676fd999b172cdd1d52459c2c72ba1e8a5e096d
unknown <natalie9705@gmail.com>
2024-10-07 20:43:51 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -111,7 +111,7 @@ public:
 		interfaceProxy.TranslateGlobalF(translationMatrix, translationVector, translationMatrix);
 		interfaceProxy.RotateYGlobalF(translationMatrix, G_DEGREE_TO_RADIAN_F(25), translationMatrix);
 
-		interfaceProxy.RotateXGlobalF(translationMatrix, G_DEGREE_TO_RADIAN_F(85), translationMatrix);
+		interfaceProxy.RotateXGlobalF(translationMatrix, G_DEGREE_TO_RADIAN_F(65), translationMatrix);
 
 		interfaceProxy.InverseF(translationMatrix, viewMatrix);
 	}
